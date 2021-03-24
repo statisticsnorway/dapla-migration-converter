f38c7e27ae7f7a81572d893564b0e062931bbc16
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2021-03-24 21:33:49 +0100

    Upgrade pubsub dependency

--- src/main/java/no/ssb/rawdata/converter/app/migration/pubsub/PubSubAdmin.java
+++ src/main/java/no/ssb/rawdata/converter/app/migration/pubsub/PubSubAdmin.java
@@ -1,5 +1,6 @@
 package no.ssb.rawdata.converter.app.migration.pubsub;
 
+import com.google.api.gax.rpc.AlreadyExistsException;
 import com.google.cloud.pubsub.v1.SubscriptionAdminClient;
 import com.google.cloud.pubsub.v1.TopicAdminClient;
 import com.google.pubsub.v1.DeadLetterPolicy;
@@ -7,10 +8,10 @@ import com.google.pubsub.v1.ListSubscriptionsRequest;
 import com.google.pubsub.v1.ListTopicsRequest;
 import com.google.pubsub.v1.ProjectName;
 import com.google.pubsub.v1.ProjectSubscriptionName;
-import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.PushConfig;
 import com.google.pubsub.v1.Subscription;
 import com.google.pubsub.v1.Topic;
+import com.google.pubsub.v1.TopicName;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -35,19 +36,19 @@ public class PubSubAdmin {
             String dlqProjectId,
             String dlqTopic) {
 
-        ProjectTopicName projectTopicName = ProjectTopicName.of(projectId, topic);
+        TopicName topicName = TopicName.of(projectId, topic);
         ProjectSubscriptionName projectSubscriptionName = ProjectSubscriptionName.of(projectId, subscriptionName);
 
         Subscription.Builder builder = Subscription.newBuilder();
         builder.setName(projectSubscriptionName.toString())
-                .setTopic(projectTopicName.toString())
+                .setTopic(topicName.toString())
                 .setPushConfig(PushConfig.getDefaultInstance())
                 .setAckDeadlineSeconds(ackDeadlineSeconds);
 
         if (dlqTopic != null) {
-            ProjectTopicName dlqProjectTopicName = ProjectTopicName.of(dlqProjectId, dlqTopic);
+            TopicName dlqtopicName = TopicName.of(dlqProjectId, dlqTopic);
             builder.setDeadLetterPolicy(DeadLetterPolicy.newBuilder()
-                    .setDeadLetterTopic(dlqProjectTopicName.toString())
+                    .setDeadLetterTopic(dlqtopicName.toString())
                     .setMaxDeliveryAttempts(maxRedeliveryAttemptsBeforeSendingToDlq)
                     .build());
         }
@@ -58,27 +59,37 @@ public class PubSubAdmin {
             return;
         }
 
-        log.info("Creating subscription: {}", projectSubscriptionName.toString());
-        subscriptionAdminClient.createSubscription(subscription);
-    }
-
-    public static void createTopicIfNotExists(TopicAdminClient topicAdminClient, String projectId, String topic) {
-        ProjectTopicName projectTopicName = ProjectTopicName.of(projectId, topic);
-        if (!topicExists(topicAdminClient, ProjectName.of(projectId), projectTopicName, 25)) {
-            log.info("Creating topic: {}", projectTopicName.toString());
-            topicAdminClient.createTopic(projectTopicName);
+        try {
+            log.info("Creating subscription: {}", projectSubscriptionName.toString());
+            subscriptionAdminClient.createSubscription(subscription);
+            log.info("Subscription created: {}", projectSubscriptionName.toString());
+        } catch (AlreadyExistsException e) {
+            log.info("Already existed, no need to create subscription: {}", projectSubscriptionName.toString());
         }
     }
 
-    public static boolean topicExists(TopicAdminClient topicAdminClient, ProjectName projectName, ProjectTopicName projectTopicName, int pageSize) {
-        log.trace("Listing topics on project: {}", projectName.getProject());
+    public static void createTopicIfNotExists(TopicAdminClient topicAdminClient, String projectId, String topic) {
+        TopicName topicName = TopicName.of(projectId, topic);
+        if (!topicExists(topicAdminClient, ProjectName.of(projectId), topicName, 25)) {
+            try {
+                log.info("Creating topic: {}", topicName.toString());
+                topicAdminClient.createTopic(topicName);
+                log.info("Topic created: {}", topicName.toString());
+            } catch (AlreadyExistsException e) {
+                log.info("Already existed, no need to create topic: {}", topicName.toString());
+            }
+        }
+    }
+
+    public static boolean topicExists(TopicAdminClient topicAdminClient, ProjectName projectName, TopicName topicName, int pageSize) {
+        log.info("Checking if topic exists: {}", topicName.toString());
         TopicAdminClient.ListTopicsPagedResponse listResponse = topicAdminClient
                 .listTopics(ListTopicsRequest.newBuilder()
                         .setProject(projectName.toString())
                         .setPageSize(pageSize)
                         .build());
         for (Topic topic : listResponse.iterateAll()) {
-            if (topic.getName().equals(projectTopicName.toString())) {
+            if (topic.getName().equals(topicName.toString())) {
                 return true;
             }
         }
@@ -90,7 +101,7 @@ public class PubSubAdmin {
                             .setPageSize(pageSize)
                             .build());
             for (Topic topic : listResponse.iterateAll()) {
-                if (topic.getName().equals(projectTopicName.toString())) {
+                if (topic.getName().equals(topicName.toString())) {
                     return true;
                 }
             }
@@ -99,7 +110,7 @@ public class PubSubAdmin {
     }
 
     public static boolean subscriptionExists(SubscriptionAdminClient subscriptionAdminClient, ProjectName projectName, ProjectSubscriptionName projectSubscriptionName, int pageSize) {
-        log.trace("Listing subscriptions on project: {}", projectName.getProject());
+        log.info("Checking if subscription exists: {}", projectSubscriptionName.toString());
         SubscriptionAdminClient.ListSubscriptionsPagedResponse listResponse = subscriptionAdminClient
                 .listSubscriptions(ListSubscriptionsRequest.newBuilder()
                         .setProject(projectName.toString())
